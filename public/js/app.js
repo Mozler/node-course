e9eca02778dab978bf8f70dc02893e7742ef718d
mozler <mozler@outlook.com>
2020-09-07 21:51:14 +0300

    test

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,22 @@ const msgOne = document.getElementById("messageOne");
 const msgTwo = document.getElementById("messageTwo");
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  msgOne.textContent = "loading...";
-  msgTwo.textContent = "";
-  fetch("/weather?address=" + searchLocation.value).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        msgOne.textContent = data.error;
-      } else {
-        msgOne.textContent = data.location;
-        msgTwo.textContent = data.temperature;
-      }
+  if (searchLocation.value.length > 0) {
+    msgOne.textContent = "loading...";
+    msgTwo.textContent = "";
+    fetch("/weather?address=" + searchLocation.value).then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          msgOne.textContent = data.error;
+        } else {
+          msgOne.textContent = data.location;
+          msgTwo.textContent = data.temperature;
+        }
+      });
     });
-  });
-  searchLocation.value = "";
+    searchLocation.value = "";
+  } else {
+    msgOne.textContent = "Please provide an address";
+    msgTwo.textContent = "";
+  }
 });
